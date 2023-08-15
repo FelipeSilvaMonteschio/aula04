e34dbdc6cde8d25c17e63f2fc4cb4c59689bd896
FelipeSilvaMonteschio <felipe.monteschio@outlook.com>
2023-08-15 17:36:47 -0400

    version1.0

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,10 +1,41 @@
 'use server'
+const responseIsuserAuth = [
+    {   name : "felipe",
+        email : "felipe@teste.com",
+        password : "1234",
+        token : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    },
+    {   
+        name : "natsha",
+        email : "natasha@teste",
+        password : "1234",
+        token : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    },
+    {   name : "karol",
+        email : "karol@teste",
+        password : "1234",
+        token : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    },
+    {   name : "marcelo",
+        email : "marcelo@teste",
+        password : "1234",
+        token : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    },
+]
 
-const getUserAuthenticated = (user) => {
+const getUserAuthenticated = (userLogin) => {
+let Userauth = {}
 
+
+    responseIsuserAuth.map((user) =>{
+        if(user.email === userLogin.email && user.password === userLogin.password){
+            Userauth= user
+        }
+    })
+    return Userauth
 }
-
 const getUsers = () =>{
-        
+    return responseIsuserAuth
+
 }
 export { getUsers, getUserAuthenticated };
--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,10 +1,18 @@
-import { getUsers } from "@/app/functions/handlerAcessAPI";
+import { getUsers } from "@/app/functions/handlerAcessAPI"
 
 export default async function Dashboard() {
-   
+   const Lista = getUsers()
     return (
         <div>
-            <h1>Dashboard</h1>
+            <h1>Lista</h1>
+
+
+            {Lista.map((user) => 
+            <div>
+                <h2> {user.name}</h2>
+                <h4> {user.password}</h4><br/><br/>
+            </div>
+            )}
             
         </div>
     );
