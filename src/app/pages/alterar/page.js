453e32216e94c5cb6b1c48ceccffef6e3014ca55
FelipeSilvaMonteschio <felipe.monteschio@outlook.com>
2023-08-29 17:19:25 -0400

    input-page-rotas

--- src/app/pages/alterar/page.js
+++ src/app/pages/alterar/page.js
@@ -1,9 +1,27 @@
-
+import styles from '../../css/style.module.css'
 
 export default function alterar() {
     return(
-        <div>
-            <h1>Alter</h1>
-        </div>
+        <form className={styles.register} >
+            <h1>Alterar</h1>
+
+            <div id="blocos">
+            <label id={styles.label}>Nome</label>
+            <input type="name" id="btnInput" placeholder="Insira seu Nome"/>
+            </div>
+
+            <div id="blocos">
+                <label id={styles.label}>Email</label>
+                <input type="email" id="btnInput" placeholder="Insira seu Email" />
+            </div>
+
+            <div id="blocos">
+                <label id={styles.label}>Senha</label>
+                <input type="password" id="btnInput" placeholder="Insira sua Senha" />
+            </div>
+
+            <button id={styles.btn}> Alterar</button>
+
+        </form>
     )
 }
--- src/app/pages/register/page.js
+++ src/app/pages/register/page.js
@@ -1,9 +1,27 @@
-
+import styles from '../../css/style.module.css'
 
 export default function register() {
     return(
-        <div>
+        <form className={styles.register} >
             <h1>Registro</h1>
-        </div>
+
+            <div id="blocos">
+            <label id={styles.label}>Nome</label>
+            <input type="name" id="btnInput" placeholder="Insira seu Nome"/>
+            </div>
+
+            <div id="blocos">
+                <label id={styles.label}>Email</label>
+                <input type="email" id="btnInput" placeholder="Insira seu Email" />
+            </div>
+
+            <div id="blocos">
+                <label id={styles.label}>Senha</label>
+                <input type="password" id="btnInput" placeholder="Insira sua Senha" />
+            </div>
+
+            <button id={styles.btn} >Cadastrar</button>
+
+        </form>
     )
 }
